17ecaac372253ff9538cbda8ad87c98b3c6fe117
Cristian Gutiérrez <ggcristian@icloud.com>
2026-02-01 13:09:45 +0100

    Clarify comment [skip ci]

--- test/s3_test.cpp
+++ test/s3_test.cpp
@@ -33,8 +33,9 @@ protected:
         // so we can re-run the suite as many times as we want
         S3Client client = S3Client("minio_access", "minio_secret", "127.0.0.1:9000", S3AddressingStyle::PathStyle);
 
-        // This buckets *are* empty, therefore we won't trigger a paginator
-        // if they fill in at some point in the future, this will throw error
+        // This buckets *are* empty, therefore there is no need for a paginator to
+        // remove the elements inside each bucket if they fill in at some point in
+        // the future, this will throw error
         std::vector<std::string> BucketsToDelete {
             "test-bucket-s3cpp",
             "test-bucket-location",
